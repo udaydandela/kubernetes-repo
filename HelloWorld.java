69556213db5fc11414d3d31174db1524342c1849
Dandela Uday kiran <udaykirandandela@gmail.com>
2025-10-15 15:27:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World uday kiran022");
         System.out.println("Hello, World bala krishna");
-        System.out.println("Hello, World akash prabhu");
+        System.out.println("Hello, World akash prabhu.k");
     }
 }
